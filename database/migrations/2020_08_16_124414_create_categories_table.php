7111a74b180de4b7d5f6c74b69f215028f8d5192
Soloviev2642a <Soloviev2642a@gmail.com>
2020-08-16 14:01:53 +0300

    migrations default value fix

--- database/migrations/2020_08_16_124414_create_categories_table.php
+++ database/migrations/2020_08_16_124414_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('parent_id');
+            $table->integer('parent_id')->default('0');
             $table->integer('external_id');
             $table->timestamp('updated_at');
         });
